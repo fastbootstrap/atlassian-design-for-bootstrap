95c2e06630b302b6aa5015cadd487e0aff8b4548
zhengchun <zhengchunster@gmail.com>
2025-08-18 23:15:08 +0800

    Remove unused `bootstrap.native` package and dependency

--- src/ts/interface/getOrCreateInstance.ts
+++
@@ -1,3 +0,0 @@
-export declare interface getOrCreateInstance<T, C = void> {
-  (element: HTMLElement, config?: C): T;
-}
--- src/ts/tooltip.ts
+++
@@ -1,7 +0,0 @@
-import { Tooltip, TooltipOptions } from 'bootstrap.native';
-
-Tooltip.getOrCreateInstance = (element: HTMLElement, config?: Partial<TooltipOptions>): Tooltip => {
-  return Tooltip.getInstance(element) ?? new Tooltip(element, config);
-};
-
-export default Tooltip;
